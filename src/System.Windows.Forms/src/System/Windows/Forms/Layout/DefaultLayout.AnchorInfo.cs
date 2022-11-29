b850083bed92a8b4ad53621ec5bbc141139088f9
Devendar Reddy Adulla <dreddy@microsoft.com>
2022-11-29 12:39:56 -0800

    Cache DisplayRect used to compute anchors
    Fixes #8266.
    
    In PermonV2 mode applications, when moved from one monitor to the other, scale factor computed with respect to new DPI on the monitor
    is not alligning with the windows SuggestedRectangle for top-level window on the monitor. To mitigate, we are calculating scale factor
    based on the chnage in DisplayRectangle, for scaling anchors.

--- src/System.Windows.Forms/src/System/Windows/Forms/Layout/DefaultLayout.AnchorInfo.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Layout/DefaultLayout.AnchorInfo.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
+
 namespace System.Windows.Forms.Layout
 {
     internal partial class DefaultLayout
@@ -12,6 +14,10 @@ namespace System.Windows.Forms.Layout
             public int Top;
             public int Right;
             public int Bottom;
+
+            // Stores display rectangle of the parent container at which anchors
+            // of the element computed.
+            public Rectangle DisplayRect;
         }
     }
 }
